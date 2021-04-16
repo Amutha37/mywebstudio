0d0b137ff508d1c99a05840c38c936a2006f7495
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2021-04-16 16:24:47 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my web studio</h1>
+        <h4>Author : Amutha </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
